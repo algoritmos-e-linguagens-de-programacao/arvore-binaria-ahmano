10f360a7eaed96f77a9213f20a736c6a5d12ca65
Enzo Traldi dos Anjos <86757859+Enzimeas@users.noreply.github.com>
2022-11-21 22:08:58 -0300

    Create Node.py

---
+++ Node.py
@@ -0,0 +1,18 @@
+class node:
+
+  def __init__(self,value,direita = None,esquerda = None):
+    self.value = value
+    self.direita = direita
+    self.esquerda = esquerda
+
+  def printar(self):
+    print(self.value)
+
+
+  def setDireita(self,direita):
+    self.direita = direita
+
+  def setEsquerda(self,esquerda):
+    self.esquerda = esquerda
+
+    
