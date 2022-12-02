e27f720cab915b2c08d620254e1efbcf1ce886e5
Enzo Traldi dos Anjos <86757859+Enzimeas@users.noreply.github.com>
2022-12-02 19:59:11 -0300

    Refazendo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from BinaryTree import binaryTree
+from binary_tree import binaryTree
 
 arvore = binaryTree()
 arvore.adicionar(4)
@@ -8,6 +8,21 @@ arvore.adicionar(10)
 arvore.adicionar(1)
 arvore.adicionar(12)
 arvore.adicionar(3)
+
+
+print("ARVORE INICIAL")
+arvore.printar()
+print("------------------------------------")
+print("PRE ORDEM")
+arvore.ex_preOrdem()
+print("------------------------------------")
+print("EM ORDEM")
+arvore.ex_emOrdem()
+print("------------------------------------")
+print("POS ORDEM")
+arvore.ex_posOrdem()
+print("------------------------------------")
+print("ARVORE INICIAL")
 arvore.printar()
 print("------------------------------------")
 print("ARVORE APOS REMOVER NUMERO 5")
@@ -17,5 +32,7 @@ print("------------------------------------")
 print("ARVORE APOS REMOVER NUMERO 2")
 arvore.remover(2)
 arvore.printar()
+
+
 
 
